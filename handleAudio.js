310d6891b0100a5da97ed9f34d898d5eebc82f93
Viewerport <250864576+Viewerport@users.noreply.github.com>
2026-03-29 09:55:52 -0500

    Add files via upload

--- handleAudio.js
+++ handleAudio.js
@@ -1,5 +1,8 @@
 let intonation = document.getElementById("intonation");
 let extraInfo = document.getElementById("extraInfo");
+let displayNote = document.getElementById("displayNote");
+const canv = document.getElementById("scale");
+const cont = canv.getContext("2d");
 
 window.addEventListener("load", async function(){
   let stream = null;
@@ -11,7 +14,7 @@ window.addEventListener("load", async function(){
     const audioCont = new AudioContext();
     const analyser = audioCont.createAnalyser();
     analyser.fftSize = 32768;
-    analyser.minDecibels = -80;
+    analyser.minDecibels = -50;
 
     const source = audioCont.createMediaStreamSource(stream);
     source.connect(analyser);
@@ -47,29 +50,50 @@ window.addEventListener("load", async function(){
       let note = notes[lowestPositive(noteIndex, noteIndex + 12)]; 
       let octave = Math.round(semitonesFromA / 12) + 4;
       let centDeviation = 1200 * (Math.log(actualFrequency / nearestNoteFrequency) / Math.log(2));
-
-      extraInfo.innerHTML = 
-        "NOTE: " + note + octave + "<br>" + 
-        "FREQUENCY: " + actualFrequency + "<br>" +
+      
+      displayNote.innerHTML = note + octave;
+      extraInfo.innerHTML = "<pre>" + 
+        "             FREQUENCY: " + actualFrequency + "<br>" +
         "NEAREST NOTE FREQUENCY: " + nearestNoteFrequency + "<br>" +
-        "SEMITONES FROM A4: " + semitonesFromA + "<br>" +
-        "CENT DEVIATION: " + centDeviation;
-
+        "     SEMITONES FROM A4: " + semitonesFromA + "<br>" +
+        "        CENT DEVIATION: " + centDeviation + "</pre>";
+      
       if (centDeviation > 0){
         intonation.innerHTML = "SHARP <br> +" + Math.abs(centDeviation) + " CENTS";
       }else
       if (centDeviation < 0){
         intonation.innerHTML = "FLAT <br> -" + Math.abs(centDeviation) + " CENTS";
+      }else
+      if (centDeviation == 0){
+        intonation.innerHTML = "PERFECTLY IN TUNE<br>" + Math.abs(centDeviation) + " CENTS";
       }else{
-        intonation.innerHTML = "AMAZING BECAUSE YOU ARE PERFECTLY IN TUNE!!!!!!<br>" + "0 CENTS";
+        intonation.innerHTML = "NOT PLAYING<br>" + "NAN CENTS";
       }
+
+      cont.clearRect(0, 0, canv.width, canv.height);
+      
+      const grad = cont.createLinearGradient(0, 0, 0, canv.height);
+      grad.addColorStop(0, "rgb(255, 0, 0)");
+      grad.addColorStop(0.5, "rgb(0, 255, 0)");
+      grad.addColorStop(1, "rgb(255, 0, 0)");
+
+      cont.fillStyle = grad;
+      cont.fillRect(0, 0, canv.width, canv.height);
+
+      cont.fillStyle = "rgb(0, 0, 0)";
+      cont.fillRect(0, canv.height / 2, canv.width, 5);
+
+      cont.fillStyle = "rgb(0, 0, 255)";
+      cont.fillRect(0, centDeviation / 50 * canv.height / 2 + canv.height / 2, canv.width, 5);
 
       requestAnimationFrame(getFrequencies);
     }
     getFrequencies();
   }catch(err){
     if (err.name == "NotAllowedError"){
-      intonation.innerHTML = 'A HORRIBLE PERSON WHO DOESNT WANT TO TUNE. I BET YOURE AT LEAST 20 CENTS SHARP RIGHT NOW AND YOURE JUST GOING TO IGNORE IT LIKE SOME LOWLY, GOOD FOR NOTHING, SCOUNDREL. IS THAT WHO YOU WANT TO BE????? THERE ARE THOUSANDS, PROBABLY MILLIONS OF OTHER PEOPLE USING A TUNER. REGARDLESS OF WHETHER THEY ARE A MUSICIAN OR NOT, THEY ARE GETTING BETTER THAN YOU. WHATS THAT YOU SAY?? YOURE WORRIED ABOUT YOUR PRIVACY??? WELL (if youre in the united states) TELL YOUR STATE REPRESENTATIVE TO VOTE AGAINST LAWS SUCH AS THE APP STORE ACCOUNTABILITY ACT. THIS LAW WOULD FORCE APP STORES, LIKE THE "APP STORE" FOR APPLE DEVICES AND THE "GOOGLE PLAY STORE" FOR GOOGLE DEVICES, TO PROVIDE A FORM TO VERIFY YOUR AGE. THE FORM WOULD MAKE YOU SUBMIT EITHER A PICTURE OF YOURSELF OR YOUR GOVERNMENT ID. IF YOU SUBMIT A PICTURE OF YOURSELF, THE APP STORE NEEDS TO SEND THAT PICTURE TO A THIRD PARTY SO THEY CAN VERIFY YOUR AGE. THAT THIRD PARTY CAN STORE THAT PICTURE OF YOU. ON THE OTHER HAND, IF YOU SUBMIT A PICTURE OF YOUR GOVERNMENT ID, THE APP STORE CAN STORE THAT AS WELL. THE PLACES THEY STORE THAT DATA ARE VULNERABLE. IN FEBRUARY OF 2026, A FACIAL VERIFICATION COMPANY KNOWN AS "PERSONA" HAD A DATA BREACH. ARE YOU WILLING PUT YOUR IDENTITY IN THE HANDS OF COMPANIES NOTORIOUS FOR THEIR EXCESSIVE SURVEILANCE????? IF NOT, EMAIL, CALL, OR TALK WITH YOUR STATE REPRESENTATIVE AND USE YOUR VOICE. OH, I DIDNT REPOND TO YOUR CONCERN????? WELL ILL HAVE YOU KNOW THAT THE AUDIO THIS WEBSITE IS RECORDING WILL NEVER BE STORED AND WILL BE DELETED WHEN YOU CLOSE THE TAB. SO REFRESH THE PAGE RIGHT THIS INSTANT, ACCEPT THE MICROPHONE USAGE PROMPT AND TUNE, YOU FUCK';
+      intonation.innerHTML = 'A HORRIBLE PERSON WHO DOESNT WANT TO TUNE. I BET YOURE AT LEAST 20 CENTS SHARP RIGHT NOW AND YOURE JUST GOING TO IGNORE IT LIKE SOME LOWLY, GOOD FOR NOTHING, SCOUNDREL. IS THAT WHO YOU WANT TO BE????? THERE ARE THOUSANDS, PROBABLY MILLIONS OF OTHER PEOPLE USING A TUNER. REGARDLESS OF WHETHER THEY ARE A MUSICIAN OR NOT, THEY ARE GETTING BETTER THAN YOU. WHATS THAT YOU SAY?? YOURE WORRIED ABOUT YOUR PRIVACY??? WELL ILL HAVE YOU KNOW THAT THE AUDIO THIS WEBSITE IS RECORDING WILL NEVER BE STORED AND WILL BE DELETED WHEN YOU CLOSE THE TAB. SO REFRESH THE PAGE RIGHT THIS INSTANT, ACCEPT THE MICROPHONE USAGE PROMPT AND TUNE, YOU FUCK';
+    }else{
+      intonation.innerHTML = err;
     }
   }
 });
